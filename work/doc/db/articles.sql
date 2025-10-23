c932792b04c92eb02a81c15160b4f2a7b3550582
chris <yeditor@gmail.com>
2025-10-23 17:39:51 +0800

    轉移至 member 增加

--- work/doc/db/articles.sql
+++ work/doc/db/articles.sql
@@ -1,16 +1,3 @@
-DROP TABLE IF EXISTS `articles` ;
-
-CREATE TABLE IF NOT EXISTS `articles` (
-    `RowID` INT AUTO_INCREMENT,
-    `Title` VARCHAR(255) NOT NULL,
-    `Content` TEXT,
-    `UpdateDt` DATETIME NOT NULL,
-    `CreatedDt` DATETIME NOT NULL,
-    `NodeID` CHAR(36) NOT NULL,
-    PRIMARY KEY (`RowID`),
-    KEY `k_node_id` (`NodeID`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
-
 -- SP
 DROP PROCEDURE IF EXISTS `sp_add_article`;
 
