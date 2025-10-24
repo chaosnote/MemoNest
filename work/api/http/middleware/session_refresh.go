bca8c4f5f3ac08e6272af3b25be35b12ff91b0ff
chris <yeditor@gmail.com>
2025-10-24 16:28:06 +0800

    刪除不使用的 log

--- work/api/http/middleware/session_refresh.go
+++ work/api/http/middleware/session_refresh.go
@@ -1,15 +1,13 @@
 package middleware
 
 import (
-	"fmt"
-	"idv/chris/MemoNest/domain/service"
-
 	"github.com/gin-gonic/gin"
+
+	"idv/chris/MemoNest/domain/service"
 )
 
 func SessionRefresh(session service.Session) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println("Update Session")
 		session.Init(c)
 		session.Refresh()
 		c.Next()
