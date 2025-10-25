2e4f197c285401398089139dc69b8316b7392d26
chris <yeditor@gmail.com>
2025-10-25 17:46:12 +0800

    調整 token 運用

--- work/api/http/handle/member_handler.go
+++ work/api/http/handle/member_handler.go
@@ -1,7 +1,9 @@
 package handle
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -27,16 +29,40 @@ func (h *MemberHandler) Login(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"Code": err.Error()})
 		}
 	}()
+
 	var param struct {
 		Account  string `json:"account" form:"account"`
 		Password string `json:"password" form:"password"`
+		Token    string `json:"token" form:"token"`
 	}
 	err = c.ShouldBind(&param)
 	if err != nil {
 		return
 	}
 
+	aes_key := utils.MD5Hash(c.ClientIP())
+	if len(param.Token) > 0 {
+		tmp, err := utils.AesDecrypt(param.Token, []byte(aes_key))
+		if err != nil {
+			return
+		}
+		list := strings.Split(tmp, "|")
+		if len(list) != 2 {
+			c.JSON(http.StatusOK, gin.H{"Code": "請使用手動登入"})
+			return
+		}
+		param.Account = list[0]
+		param.Password = list[1]
+	}
+
+	logger.Info(msg, zap.Any("params", param))
+
 	_, err = h.UC.Login(param.Account, param.Password, c.ClientIP())
+	if err != nil {
+		return
+	}
+
+	token, err := utils.AesEncrypt([]byte(fmt.Sprintf("%s|%s", param.Account, param.Password)), []byte(aes_key))
 	if err != nil {
 		return
 	}
@@ -44,7 +70,11 @@ func (h *MemberHandler) Login(c *gin.Context) {
 	h.Session.Init(c)
 	h.Session.SetAccount(param.Account, c.ClientIP())
 
-	c.JSON(http.StatusOK, gin.H{"Code": "OK", "message": ""})
+	c.JSON(http.StatusOK, gin.H{
+		"Code":    "OK",
+		"message": "",
+		"token":   token,
+	})
 }
 
 // 使用者登出
@@ -73,6 +103,8 @@ func (h *MemberHandler) Register(c *gin.Context) {
 	if err != nil {
 		return
 	}
+
+	logger.Info(msg, zap.Any("params", param))
 
 	_, err = h.UC.Register(param.Account, param.Password, c.ClientIP())
 	if err != nil {
